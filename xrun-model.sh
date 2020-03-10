1a99a28b55ff52cafb6bddc9452ad0c1d551667f
sopeeweje <58275563+sopeeweje@users.noreply.github.com>
2020-03-10 11:28:52 -0400

    Run t1c- and t2-features

--- xrun-model.sh
+++ xrun-model.sh
@@ -1,4 +1,2 @@
-
-pipenv run python xrun.py --trials 10 --description $1 --model v2 --form t1 --label outcome --hyperparameters xhyperparameters.json --split 62e0fad6-783f-4453-af2f-a52b241f1188
-pipenv run python xrun.py --trials 10 --description $1 --model v2 --form t2 --label outcome --hyperparameters xhyperparameters.json --split 62e0fad6-783f-4453-af2f-a52b241f1188
-pipenv run python xrun.py --trials 10 --description $1 --model v2 --form features --label outcome --hyperparameters xhyperparameters.json --split 62e0fad6-783f-4453-af2f-a52b241f1188
+pipenv run python xrun.py --trials 10 --description $1 --model v2 --form t1c-features --label outcome --hyperparameters xhyperparameters.json --split 27863f52-d15d-4cc1-be66-155872f20d6b 
+pipenv run python xrun.py --trials 10 --description $1 --model v2 --form t2-features --label outcome --hyperparameters xhyperparameters.json --split 27863f52-d15d-4cc1-be66-155872f20d6b

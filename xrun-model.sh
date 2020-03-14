ad49a567c75d78780eb26f2b334b0a2d6c7cf418
Sope Eweje <feyisope@mit.edu>
2020-03-14 12:22:55 -0400

    Crossval update

--- xrun-model.sh
+++ xrun-model.sh
@@ -1,2 +1,2 @@
-pipenv run python xrun.py --trials 10 --description $1 --model v2 --form t1c-features --label outcome --hyperparameters xhyperparameters.json #--split
+pipenv run python xrun.py --description $1 --model v2 --form t1c-features --label outcome --hyperparameters xhyperparameters.json #--split
 #pipenv run python xrun.py --trials 10 --description $1 --model v2 --form t2-features --label outcome --hyperparameters xhyperparameters.json --split

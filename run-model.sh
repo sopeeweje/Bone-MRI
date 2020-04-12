687003a739d36ed1df3d0a9994f1355aeb71bf54
sopeeweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-04-12 11:25:38 -0700

    Changed run to v2'

--- run-model.sh
+++ run-model.sh
@@ -1,4 +1,4 @@
-pipenv run python run.py --description $1 --model v4 --form t1c-features --label outcome --hyperparameters hyperparameters.json #--split 4860bb0e-11d4-493c-b9b0-bb8822952f71 #27863f52-d15d-4cc1-be66-155872f20d6b #b888e896-e151-4224-8b4e-12d36f6307c8
+pipenv run python run.py --description $1 --model v2 --form t1c-features --label outcome --hyperparameters hyperparameters.json #--split 4860bb0e-11d4-493c-b9b0-bb8822952f71 #27863f52-d15d-4cc1-be66-155872f20d6b #b888e896-e151-4224-8b4e-12d36f6307c8
 #pipenv run python run.py --description $1 --model v3 --form t1c-features --label outcome --hyperparameters hyperparameters.json --split 4860bb0e-11d4-493c-b9b0-bb8822952f71
 
 #change form to get different combos of input features, can be imaging alone, clinical alone, or both

aaac2feacbe840f84184aa931cbd04479e76504f
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-04-12 17:39:09 -0400

    set up experiment with v4

--- xrun-model.sh
+++ xrun-model.sh
@@ -1,2 +1,7 @@
-pipenv run python xrun.py --description $1 --model v2 --form t1c-features --label outcome --hyperparameters xhyperparameters.json #--split
+pipenv run python xrun.py --description $1 --model v4 --form t1c-t2 --label outcome --hyperparameters xhyperparameters.json #--split
+
+pipenv run python xrun.py --description $1 --model v4 --form t1c-features --label outcome --hyperparameters xhyperparameters.json #--split
+
+pipenv run python xrun.py --description $1 --model v4 --form t2-features --label outcome --hyperparameters xhyperparameters.json #--split
+
 #pipenv run python xrun.py --trials 10 --description $1 --model v2 --form t2-features --label outcome --hyperparameters xhyperparameters.json --split

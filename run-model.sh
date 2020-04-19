2631330a5ed220776d9f7ab4661fe4f8200c0e0f
sopeeweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-04-19 08:14:13 -0700

    Results

--- run-model.sh
+++ run-model.sh
@@ -1,4 +1,4 @@
-pipenv run python run.py --description $1 --model v2 --form t1 --label outcome --hyperparameters hyperparameters.json --split bf7b9854-626a-4613-916a-1e09ed01306
+pipenv run python run.py --description $1 --model v2 --form t1 --label outcome --hyperparameters hyperparameters.json --split bf7b9854-626a-4613-916a-1e09ed013067
 
 pipenv run python run.py --description $1 --model v2 --form t1c --label outcome --hyperparameters hyperparameters.json --split 46ea3109-a0d3-4e27-9e0b-22f09d2f2fd7
 

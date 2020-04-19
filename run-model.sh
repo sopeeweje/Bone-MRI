3657750c6e0e9acf58cec4b741e46592373c102c
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-04-18 22:20:52 -0400

    Set up final runs

--- run-model.sh
+++ run-model.sh
@@ -1,12 +1,5 @@
-pipenv run python run.py --description $1 --model v4 --form t1c-t2 --label outcome --hyperparameters hyperparameters.json #--split 4860bb0e-11d4-493c-b9b0-bb8822952f71
+pipenv run python run.py --description $1 --model v2 --form t1 --label outcome --hyperparameters hyperparameters.json --split bf7b9854-626a-4613-916a-1e09ed01306
 
-pipenv run python run.py --description $1 --model v4 --form t1c-features --label outcome --hyperparameters hyperparameters.json #--split 4860bb0e-11d4-493c-b9b0-bb8822952f71
+pipenv run python run.py --description $1 --model v2 --form t1c --label outcome --hyperparameters hyperparameters.json --split 46ea3109-a0d3-4e27-9e0b-22f09d2f2fd7
 
-pipenv run python run.py --description $1 --model v4 --form t2-features --label outcome --hyperparameters hyperparameters.json #--split 4860bb0e-11d4-493c-b9b0-bb8822952f71
-
-#pipenv run python run.py --description $1 --model v3 --form t1c-features --label outcome --hyperparameters hyperparameters.json --split 4860bb0e-11d4-493c-b9b0-bb8822952f71
-
-#change form to get different combos of input features, can be imaging alone, clinical alone, or both
-
-#pipenv run python run.py --description $1 --model v2 --form t2 --label outcome --hyperparameters hyperparameters.json --split 6f91b43f-d6cd-42dc-9eb6-8178f4ea8cc1
-#pipenv run python run.py --description $1 --model v2 --form features --label outcome --hyperparameters hyperparameters.json --split 6f91b43f-d6cd-42dc-9eb6-8178f4ea8cc1
+pipenv run python run.py --description $1 --model v2 --form t2 --label outcome --hyperparameters hyperparameters.json --split 86aaf946-fb7d-4402-b725-b9959e0f004a

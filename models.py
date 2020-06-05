5b23c12c58d9c98d6d233729caa3c69fd8bb827d
sopeeweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-06-05 16:24:24 -0700

    resutls

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-#import v1
+import v1
 import v2
 import v3
 import v4
 
 models = {
-#    "v1": v1,
+    "v1": v1,
     "v2": v2,
     "v3": v3,
     "v4": v4
--- xrun-model.sh
+++ xrun-model.sh
@@ -1,6 +1,6 @@
-pipenv run python xrun.py --description $1 --model v1 --form t2 --label outcome_pos --hyperparameters xhyperparameters.json --split 84a64c17-fe3e-440c-aaaf-e1bd5b02576f
+#pipenv run python xrun.py --description $1 --model v1 --form t2 --label outcome_pos --hyperparameters xhyperparameters.json --split 84a64c17-fe3e-440c-aaaf-e1bd5b02576f
 
-pipenv run python xrun.py --description $1 --model v1 --form t1 --label outcome_pos --hyperparameters xhyperparameters.json --split 84a64c17-fe3e-440c-aaaf-e1bd5b02576f
+#pipenv run python xrun.py --description $1 --model v1 --form t1 --label outcome_pos --hyperparameters xhyperparameters.json --split 84a64c17-fe3e-440c-aaaf-e1bd5b02576f
 
 pipenv run python xrun.py --description $1 --model v2 --form t2 --label outcome_pos --hyperparameters xhyperparameters.json --split 84a64c17-fe3e-440c-aaaf-e1bd5b02576f
 

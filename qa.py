62b4418d977363bd04597baf5656e777f3f83b93
sopeeweje <feyisope.eweje@pennmedicine.upenn.edu>
2020-06-11 08:27:41 -0700

    Updated qa

--- qa.py
+++ qa.py
@@ -71,15 +71,9 @@ def processFiles(path):
     Returns: all png's to folder to look through for QA
     """
     save_path = "bone_qa/"
-    chopIDs = open("chopIDs.csv")
-    chopIDs_list = list(csv.reader(chopIDs))
-    chopIDs_dict = {}
-    
-    for combo in chopIDs_list:
-        chopIDs_dict[combo[1]] = combo[0]
     messed_up = []
     
-    available_sequences = {'t1':[],'t2':[],'t1c':[],'pd':[], 'none':[], 't2-t1c':[], 't2-t1':[]}
+    available_sequences = {'t1':[],'t2':[],'t1c':[],'pd':[], 'none':[], 't2-t1c':[], 't2-t1':[], 'all_patients':[]}
     
     print("Loading images...")
     
@@ -98,88 +92,47 @@ def processFiles(path):
         data_source = os.listdir(path + "/" + source) #all patients from particular source 
         num_patients = float(len(data_source))
         print("Number of patients: " + str(num_patients))
-#        completed = 0
-#        threshold = 10
+        completed = 0
+        threshold = 10
         
         for data_patient in data_source: #for each patient
             total_patients += 1
             if data_patient == '.DS_Store' or data_patient == '._.DS_Store':#or data_patient in SKIP:
                 continue
-            
-            patientID = data_patient #.replace("bone","").replace("-","")
-            
-#            print("Patient: " + patientID)
             patient_views = os.listdir(path + "/" + source + "/" + data_patient)
             added_views = 0
             duplicate = 0
             for view in patient_views:
                 if view == '.DS_Store':
                     continue
-                old_view = view
-                
                 try:
-                    files = os.listdir(path+"/"+source+"/"+data_patient+"/"+old_view)
-                    if not (("imagingVolume.nrrd" in files) and ("segMask_tumor.nrrd" in files)):
-                       if "segMask_GTV.nrrd" in files:
-                           os.rename(path+"/"+source+"/"+data_patient+"/"+old_view+"/"+"segMask_GTV.nrrd", path+"/"+source+"/"+data_patient+"/"+old_view+"/"+"segMask_tumor.nrrd")
-                       elif "imagingVolume.nhdr" in files:
-                           os.rename(path+"/"+source+"/"+data_patient+"/"+old_view+"/"+"imagingVolume.nhdr", path+"/"+source+"/"+data_patient+"/"+old_view+"/"+"imagingVolume.nrrd")
-                       elif "imageVolume.nrrd" in files:
-                           os.rename(path+"/"+source+"/"+data_patient+"/"+old_view+"/"+"imageVolume.nrrd", path+"/"+source+"/"+data_patient+"/"+old_view+"/"+"imagingVolume.nrrd")
-                       else:
-                           print("{}, {}".format(patientID, old_view))
-                except:
-                    pass
-
-                for char in view:
-                    if char not in ["t", "1", "2", "c", "p", "d", "w", "i"]:
-                         view = view.replace(char,"")
-                try:
-                    os.rename(path+"/"+source+"/"+data_patient+"/"+old_view, path+"/"+source+"/"+data_patient+"/"+view)
-                except:
-                    if os.path.isdir(path+"/"+source+"/"+data_patient+"/"+old_view):
-                        shutil.rmtree(path+"/"+source+"/"+data_patient+"/"+old_view)
-                    else:
-                        os.remove(path+"/"+source+"/"+data_patient+"/"+old_view)
-                try:
-                    if patientID not in available_sequences[view]:
-                        available_sequences[view].append(patientID)
+                    if data_patient not in available_sequences[view]:
+                        available_sequences[view].append(data_patient)
                         added_views += 1 
                 except:
                     pass
-                    #print(patientID)
-                    #print(view)
-                
+                                        
+                #imageVolume = path + "/" + source + "/" + data_patient + "/" + view + "/" + "imagingVolume.nrrd"
+                #segMask = path + "/" + source + "/" + data_patient + "/" + view + "/" + "segMask_tumor.nrrd"
+                #if not (os.path.isfile(imageVolume) and os.path.isfile(segMask)):
+                    #continue
+                #try:
+                    #images = [load_image(imageVolume, segMask, i) for i in range(3)]
+                #except:
+                    #messed_up.append(data_patient)
+                    #continue
+                #if not os.path.isfile(save_path+data_patient+"_"+view+"_"+"0.png"): 
+                    #plt.imsave(save_path+data_patient+"_"+view+"_"+"0.png", images[0])
+                    #plt.imsave(save_path+data_patient+"_"+view+"_"+"1.png", images[1])
+                    #plt.imsave(save_path+data_patient+"_"+view+"_"+"2.png", images[2])
+            
+            available_sequences['all_patients'].append(data_patient)
             if not added_views:
-                available_sequences['none'].append(patientID)           
-#                new_image_path = save_path+"/"+patientID+"/"+view
-#                os.makedirs(new_image_path)
-#                
-#                imageVolume = path + "/" + source + "/" + data_patient + "/" + view + "/" + "imagingVolume.nrrd"
-#                segMask = path + "/" + source + "/" + data_patient + "/" + view + "/" + "segMask_tumor.nrrd"
-#                if not (os.path.isfile(imageVolume) and os.path.isfile(segMask)):
-#                    continue
-#                try:
-#                    images = [load_image(imageVolume, segMask, i) for i in range(3)]
-#                except:
-#                    messed_up.append(data_patient)
-#                    continue
-#              
-#                if source != "CHOP":
-#                # use for non CHOP files
-#                    plt.imsave(new_image_path+"/"+patientID+"_"+view+"_"+"0.png", images[0])
-#                    plt.imsave(new_image_path+"/"+patientID+"_"+view+"_"+"1.png", images[1])
-#                    plt.imsave(new_image_path+"/"+patientID+"_"+view+"_"+"2.png", images[2])
-#                
-#                if source == "CHOP":
-#                # use for CHOP files
-#                    plt.imsave(new_image_path+"/"+patientID+"_"+view.split(" ")[0]+"_"+"0.png", images[0])
-#                    plt.imsave(new_image_path+"/"+patientID+"_"+view.split(" ")[0]+"_"+"1.png", images[1])
-#                    plt.imsave(new_image_path+"/"+patientID+"_"+view.split(" ")[0]+"_"+"2.png", images[2])
-#                completed = completed + 1
-#                if completed/num_patients*100 > threshold:
-#                    print(str(threshold) + "% complete")
-#                    threshold = threshold + 10
+                available_sequences['none'].append(patientID)  
+            completed = completed + 1
+            if completed/num_patients*100 > threshold:
+                print(str(threshold) + "% complete")
+                threshold = threshold + 10
                 
     keys = sorted(available_sequences.keys())
     t1_available = available_sequences['t1']

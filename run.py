491552d0dc7c5ac813f0cf7cd98c5f6bdf3016b5
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-01-08 16:39:35 -0500

    test runs

--- run.py
+++ run.py
@@ -60,7 +60,7 @@ def test_model(model, train, validation, test, label_form):
 def characterize_data(data):
     unique, counts = np.unique(data.classes, return_counts=True)
     index_to_count = dict(zip(unique, counts))
-    characterization = { c: index_to_count[data.class_indices[c]] for c in data.class_indices }
+    characterization = { int(c): index_to_count[data.class_indices[c]] for c in data.class_indices }
     return characterization
 
 def run(model, description, input_form, label_form="outcome", split_id=None, loaded_data=None, hyperparameters=dict()):

56f39505b5f168b52ede57022ae9afacea182921
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-03-27 21:57:21 -0400

    Restrict to patients with T1C

--- data_gen.py
+++ data_gen.py
@@ -430,6 +430,10 @@ def generate_from_features(df, input_form=config.INPUT_FORM, label_form="outcome
                 if verbose:
                     print(SHAPES_OUTPUT.format("t1"))
                 t1_masked = load_image(t1_image_file, t1_seg_file, verbose=verbose)
+                print("I'm doing the thing.")
+                if verbose:
+                    print(SHAPES_OUTPUT.format("t1c"))
+                t1c_masked = load_image(t1c_image_file, t1c_seg_file, verbose=verbose)
             except Exception as e:
                 print()
                 print("#" * 80)
@@ -444,6 +448,10 @@ def generate_from_features(df, input_form=config.INPUT_FORM, label_form="outcome
                 if verbose:
                     print(SHAPES_OUTPUT.format("t2"))
                 t2_masked = load_image(t2_image_file, t2_seg_file, verbose=verbose)
+                print("I'm doing the thing.")
+                if verbose:
+                    print(SHAPES_OUTPUT.format("t1c"))
+                t1c_masked = load_image(t1c_image_file, t1c_seg_file, verbose=verbose)
             except Exception as e:
                 print()
                 print("#" * 80)
@@ -453,6 +461,20 @@ def generate_from_features(df, input_form=config.INPUT_FORM, label_form="outcome
                 continue
 
         if parameters["t1c"]:
+            try:
+                print("I'm doing the thing.")
+                if verbose:
+                    print(SHAPES_OUTPUT.format("t1c"))
+                t1c_masked = load_image(t1c_image_file, t1c_seg_file, verbose=verbose)
+            except Exception as e:
+                print()
+                print("#" * 80)
+                print("Exception occurred for: {}\n{}".format(row, e))
+                print("T1C image unavailable")
+                print(traceback.format_exc())
+                continue
+        
+        if parameters["features"]:
             try:
                 print("I'm doing the thing.")
                 if verbose:

7ab90760dac7ba664a5225805919aa4850df6d9d
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-03-30 16:17:44 -0400

    Update clinical_data_models.py

--- clinical_data_models.py
+++ clinical_data_models.py
@@ -147,6 +147,7 @@ def features_run(label_form, classifier, split_id=None, model="n/a"):
         history.append(score)
 
     print(best_model.coef_)
+    print(best_model.score(test_set, test_labels))
     #tree_plot = plt.figure(2)
     #tree.plot_tree(best_model)
     #tree_plot.savefig("/Volumes/external/featuremodel.png")

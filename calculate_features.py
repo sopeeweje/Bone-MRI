6b9ef733007ac3f1f67ff74a448ade29ae95d08c
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-04-01 13:37:06 -0400

    reduce features

--- calculate_features.py
+++ calculate_features.py
@@ -45,29 +45,6 @@ image_feature_functions = {
 }
 
 def get_location(location):
-    # all_locations_map = {
-    #         "Clavicle": 0,
-    #         "Cranium": 1,
-    #         "Proximal femur": 2,
-    #         "Distal femur": 3,
-    #         "Foot": 4,
-    #         "Proximal radius": 5,
-    #         "Distal radius": 5,
-    #         "Proximal ulna": 5,
-    #         "Distal ulna": 5,
-    #         "Hand": 6,
-    #         "Hip": 7,
-    #         "Proximal humerus": 8,
-    #         "Distal humerus": 9,
-    #         "Proximal tibia": 10,
-    #         "Distal tibia": 11,
-    #         "Proximal fibula": 10,
-    #         "Distal fibula": 11,
-    #         "Mandible": 12,
-    #         "Rib/Chest wall": 13,
-    #         "Scapula": 14,
-    #         "Spine": 15,
-    #     }
     all_locations_map = {
             "Clavicle": 0,
             "Cranium": 1,
@@ -75,22 +52,45 @@ def get_location(location):
             "Distal femur": 3,
             "Foot": 4,
             "Proximal radius": 5,
-            "Distal radius": 6,
-            "Proximal ulna": 7,
-            "Distal ulna": 8,
-            "Hand": 9,
-            "Hip": 10,
-            "Proximal humerus": 11,
-            "Distal humerus": 12,
-            "Proximal tibia": 13,
-            "Distal tibia": 14,
-            "Proximal fibula": 15,
-            "Distal fibula": 16,
-            "Mandible": 17,
-            "Rib/Chest wall": 18,
-            "Scapula": 19,
-            "Spine": 20,
+            "Distal radius": 5,
+            "Proximal ulna": 5,
+            "Distal ulna": 5,
+            "Hand": 6,
+            "Hip": 7,
+            "Proximal humerus": 8,
+            "Distal humerus": 9,
+            "Proximal tibia": 10,
+            "Distal tibia": 11,
+            "Proximal fibula": 10,
+            "Distal fibula": 11,
+            "Mandible": 12,
+            "Rib/Chest wall": 13,
+            "Scapula": 14,
+            "Spine": 15,
         }
+    # all_locations_map = {
+    #         "Clavicle": 0,
+    #         "Cranium": 1,
+    #         "Proximal femur": 2,
+    #         "Distal femur": 3,
+    #         "Foot": 4,
+    #         "Proximal radius": 5,
+    #         "Distal radius": 6,
+    #         "Proximal ulna": 7,
+    #         "Distal ulna": 8,
+    #         "Hand": 9,
+    #         "Hip": 10,
+    #         "Proximal humerus": 11,
+    #         "Distal humerus": 12,
+    #         "Proximal tibia": 13,
+    #         "Distal tibia": 14,
+    #         "Proximal fibula": 15,
+    #         "Distal fibula": 16,
+    #         "Mandible": 17,
+    #         "Rib/Chest wall": 18,
+    #         "Scapula": 19,
+    #         "Spine": 20,
+    #     }
     # all_locations_map = {
     #         "Clavicle": 0,
     #         "Cranium": 1,
@@ -107,8 +107,8 @@ def get_location(location):
     #         "Scapula": 11,
     #         "Spine": 12
     #     }
-    all_locations_int = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    #all_locations_int = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    #all_locations_int = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    all_locations_int = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     all_locations_1hot = to_categorical(all_locations_int) #make one-hot vector for each location
     this_location = all_locations_1hot[all_locations_map[location]] #pulls one-hot vector for input location
     return list(this_location)

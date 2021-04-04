ca778304669b1869b5ab5972ad30027646c4a503
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-04-03 23:03:26 -0400

    Update clinical_data_models.py

--- clinical_data_models.py
+++ clinical_data_models.py
@@ -131,7 +131,7 @@ def features_run(label_form, classifier, split_id=None, model="n/a"):
 
     train_set, train_labels, val_set, val_labels, test_set, test_labels, external_set, external_labels = features_data(training_data, validation_data, testing_data, external_data)
     
-    for j in range(1,24):
+    for j in range(1,2):
         run_id = uuid4()
         history = []
         best_acc = -100
@@ -199,7 +199,7 @@ def features_run(label_form, classifier, split_id=None, model="n/a"):
         print("Number of Features: {}, Validation AUC: {}".format(str(j), str(roc_auc_score(val_labels, probabilities))))
         print("Rankings: {}".format(best_model.ranking_))
         #print("Coefficients: {}".format(best_model.estimator.feature_importances_))
-        print("Coefficients: {}".format(best_model.estimator_.coef_))
+        print("Coefficients: {}".format(best_model.importance_getter))
         print("")
 if __name__ == '__main__':
     #features_run("outcome_pos", LogisticRegression, UUID("84a64c17-fe3e-440c-aaaf-e1bd5b02576f"), "logistic regression")

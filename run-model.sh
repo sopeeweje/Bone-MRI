b81a90e2b7e5a8dbdc67bda66ec7b848c18ae6aa
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-04-04 14:15:56 -0400

    Update run-model.sh

--- run-model.sh
+++ run-model.sh
@@ -1,4 +1,4 @@
-python3 run.py --description $1 --model v2 --form t1c --label outcome_pos --hyperparameters hyperparameters.json --trials 10 --split 84a64c17-fe3e-440c-aaaf-e1bd5b02576f
+python3 run.py --description $1 --model v2 --form t1 --label outcome_pos --hyperparameters hyperparameters.json --trials 10 --split 84a64c17-fe3e-440c-aaaf-e1bd5b02576f
 
 #pipenv run python run.py --description $1 --model v2 --form t2 --label outcome_neg --hyperparameters hyperparameters.json --trials 1 --split 84a64c17-fe3e-440c-aaaf-e1bd5b02576f
 

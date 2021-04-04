6042f8bb0d07baca562d1f63f86f9d9c16c15c81
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-04-03 20:06:19 -0400

    gradient boosting

--- clinical_data_models.py
+++ clinical_data_models.py
@@ -196,11 +196,13 @@ def features_run(label_form, classifier, split_id=None, model="n/a"):
         pickle.dump(best_model, open(filename, 'wb'))
         
         print("Number of Features: {}, Validation AUC: {}".format(str(j), str(roc_auc_score(val_labels, probabilities))))
-        print("Rankings: {}".format(best_model.ranking_))
-        print("Coefficients: {}".format(best_model.estimator_.coef_))
+        #print("Rankings: {}".format(best_model.ranking_))
+        print("Coefficients: {}".format(best_model.estimator.feature_importances_))
+        #print("Coefficients: {}".format(best_model.estimator_.coef_))
         print("")
 if __name__ == '__main__':
-    features_run("outcome_pos", LogisticRegression, UUID("84a64c17-fe3e-440c-aaaf-e1bd5b02576f"), "logistic regression")
+    #features_run("outcome_pos", LogisticRegression, UUID("84a64c17-fe3e-440c-aaaf-e1bd5b02576f"), "logistic regression")
+    features_run("outcome_pos", GradientBoostingClassifier, UUID("84a64c17-fe3e-440c-aaaf-e1bd5b02576f"), "gradient boosting")
     #features_run("outcome_pos", MLPClassifier, UUID("84a64c17-fe3e-440c-aaaf-e1bd5b02576f"), "mlp")
     #features_run("outcome_pos", SVC, UUID("84a64c17-fe3e-440c-aaaf-e1bd5b02576f"), "support vector machine")
     #features_run("outcome_pos", tree.DecisionTreeClassifier, UUID("84a64c17-fe3e-440c-aaaf-e1bd5b02576f"), "decision tree")
